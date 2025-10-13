fa862955d1656aedaf723f1b7ff25bf5d56669b2
Theerawit Waithayawan <pm.theerawit@gmail.com>
2025-10-13 20:39:07 +0700

    build: output to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Theerawit_Resume/',
+  build: { outDir: 'docs' },    
   plugins: [react()],
-  build: { outDir: 'docs' }, 
-  base: "/Theerawit_Resume/",
-});
+})
